3b794f6558e5764ce207c90e3afcc56ec92988ff
Tawnee Harris <tawneeh@icloud.com>
2021-02-08 20:36:56 -0800

    add code from lesson 23

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -12,22 +12,25 @@ class TicketControl extends React.Component {
   }
 
   handleClick = () => { // arrow notation here is very important
-    this.setState({formVisibleOnPage: true});
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render() {
     let currentlyVisibleState = null;
-    let addTicketButton = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List";
     } else {
       currentlyVisibleState = <TicketList />
-      addTicketButton = <button onClick={this.handleClick}>Add Ticket</button>
+      buttonText = "Add Ticket";
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        {addTicketButton}
+        {<button onClick={this.handleClick}>{buttonText}</button>}
       </React.Fragment>
     );
   }
