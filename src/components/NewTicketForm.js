1ed454cca110c6cbca14a1b4401da5673cdc2a1e
Tawnee Harris <tawneeh@icloud.com>
2021-02-09 20:56:57 -0800

    add code from lesson 29

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { v4 } from "uuid";
+import PropTypes from "prop-types";
 
-function NewTicketForm() {
+function NewTicketForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={handleNewTicketFormSubmission}>
@@ -24,10 +25,12 @@ function NewTicketForm() {
 
     function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    console.log(event.target.names.value);
-    console.log(event.target.location.value);
-    console.log(event.target.issue.value);
+    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
     }
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,7 +7,8 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      masterTicketList: []
     };
   }
 
@@ -17,14 +18,20 @@ class TicketControl extends React.Component {
     }));
   }
 
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+    this.setState({masterTicketList: newMasterTicketList,
+                  formVisibleOnPage: false });
+  }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} />
       buttonText = "Add Ticket";
     }
     return (
